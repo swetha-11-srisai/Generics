5c851bfd572be62690f64c73a9e5fa44a335cc63
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-19 13:57:27 -0800

    [Swetha] add : maximum string number

--- Generics/Generics/MaxThreeVariables.cs
+++ Generics/Generics/MaxThreeVariables.cs
@@ -50,7 +50,7 @@ namespace Generics
             }
             else
             {
-                Console.WriteLine("String Three is greater");
+                Console.WriteLine("String Three is  greater");
             }
 
         }
